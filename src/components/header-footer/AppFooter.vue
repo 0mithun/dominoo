6c9a0be7de1cccc4cba404e9d58893e7a020e520
Mithun Halder <mithunrptc@gmail.com>
2019-04-27 16:15:11 +0600

    Creating Header & Footer components

---
+++ src/components/header-footer/AppFooter.vue
@@ -0,0 +1,23 @@
+<template>
+   <footer>
+       <div class="container">
+           <div>
+               <img src="" alt="">
+               <div class="contact">
+                   <div>Call Us</div>
+                   <div>8801828307745</div>
+               </div>
+           </div>
+       </div>
+   </footer>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/header-footer/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <div class="container clear">
+           <div class="logo">
+               <img :src="LogoIcon" alt="">
+           </div>
+            <div class="nav">
+                <img :src="MenuIcon" alt="">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    import  LogoIcon from '../../assets/images/logo.png'
+    import  MenuIcon from '../../assets/images/menu.png'
+    export default {
+        data() {
+            return {
+                LogoIcon,
+                MenuIcon
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
